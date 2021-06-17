71062738f925e378ff730da5b62223e538a16bb2
Amir Mir <mir-am@hotmail.com>
2021-06-17 10:37:28 +0200

    Querying Type4Py for type predicitions in a Flask app [WIP]

--- type-server/app.py
+++ type-server/app.py
@@ -1,30 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
+from type4py.infer import PretrainedType4Py, type_annotate_file
 app = Flask(__name__)
+
+t4py_pretrained_m = None
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/upload', methods = ['POST'])
+@app.before_first_request
+def load_type4py_model():
+    global t4py_pretrained_m
+    t4py_pretrained_m =  PretrainedType4Py("/home/amir/MT4Py_typed_full/type4py_pretrained/")
+    t4py_pretrained_m.load_pretrained_model()
+
+@app.route('/predict', methods = ['POST', 'GET'])
 def upload():
     """
     POST method for uploading a file. Reads in a sent file and returns it.
     TODO: modify to your own needs
     """
-    # Get files from request
-    files = request.files
+    global t4py_pretrained_m
 
-    for f in files:
-        # Get sent file
-        sent_file = files[f]
-
-        # Read source
-        # TODO: Handle (potential) security issues
-        source = sent_file.read()
-
-        # TODO: handle file processing logic here
-
-        # Only one file expected; return first
-        return source
-    
-    return '?'
+    src_file = request.data
+    print("TC", request.args.get("tc"))
+    #type_annotate_file(t4py_pretrained_m, src_file, None)
+    return type_annotate_file(t4py_pretrained_m, src_file, None)

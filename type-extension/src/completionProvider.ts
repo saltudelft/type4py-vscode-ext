a16233c5aa0d1987e95a174cda361e0b350a89ff
Evaldas Latoskinas <evaldasl61@gmail.com>
2021-07-07 18:29:12 +0200

    Tidy completionProvider.ts

--- type-extension/src/completionProvider.ts
+++ type-extension/src/completionProvider.ts
@@ -19,42 +19,11 @@ import { isWithinLineBounds } from "./utils";
 
 export abstract class CompletionProvider {
 
-    protected itemSortPrefix: number = 90;
-
-    /**
-     * Push type hints to the end of an array of completion items.
-     */
-    protected pushHintsToItems(typeHints: string[], completionItems: CompletionItem[], firstItemSelected: boolean) {
-        const sortTextPrefix = this.itemSortPrefix.toString();
-        completionItems.push(
-            firstItemSelected 
-                ? this.selectedCompletionItem(typeHints[0])
-                : this.newCompletionitem(typeHints[0], sortTextPrefix)
-        );
-
-        for (let i = 1; i < typeHints.length; i++) {
-            completionItems.push(this.newCompletionitem(typeHints[i], sortTextPrefix));
-        }
-    }
-
-    private newCompletionitem = (hint: string, sortTextPrefix: string): CompletionItem => {
-        const item = new CompletionItem(this.labelFor(hint), CompletionItemKind.TypeParameter);
-        item.sortText = sortTextPrefix + hint;
-        return item;
-    };
-
-    protected selectedCompletionItem(typeHint: string, sortTextPrefix: string = "0b"): CompletionItem {
-        let item = new CompletionItem(this.labelFor(typeHint), CompletionItemKind.TypeParameter);
-        item.sortText = `${sortTextPrefix}${typeHint}`;
-        item.preselect = true;
-        return item;
-    }
-
     protected labelFor(typeHint: string): string {
         return " " + typeHint;
     }
 
-    abstract async provideCompletionItems(
+    abstract provideCompletionItems(
         doc: TextDocument, 
         pos: Position,
         token: CancellationToken,
@@ -77,33 +46,30 @@ export class ParamHintCompletionProvider extends CompletionProvider implements C
         token: CancellationToken,
         context: CompletionContext
     ): Promise<CompletionList | null> {
-        if (context.triggerCharacter === paramHintTrigger) {
-            const items: CompletionItem[] = [];
-            const line = doc.lineAt(pos);
-            const precedingText = line.text.substring(0, pos.character - 1).trim();
-            
-            if (this.shouldProvideItems(precedingText, pos, doc) && !token.isCancellationRequested) {
-                const param = this.getParam(precedingText);
+        // Check if triggered with the correct parameter annotation character
+        if (context.triggerCharacter !== paramHintTrigger) {
+            return null;
+        }
 
-                if (param && !token.isCancellationRequested) {
-                    const inferData = findFunctionInferenceDataForActiveFilePos(pos)
+        const items: CompletionItem[] = [];
+        const line = doc.lineAt(pos);
+        const precedingText = line.text.substring(0, pos.character - 1).trim();
+        
+        // Check if pattern matches parameters
+        if (this.shouldProvideItems(precedingText, pos, doc) && !token.isCancellationRequested) {
+            // Extract parameter
+            const param = this.getParam(precedingText);
+            if (param && !token.isCancellationRequested) {
+                const inferData = findFunctionInferenceDataForActiveFilePos(pos);
 
-                    if (inferData) {
-                        const paramTypes = inferData.params[param];
-                        if (paramTypes) {
-                            for (let i = 0; i < paramTypes.length; ++i) {
-                                const item = new CompletionItem(paramTypes[i], CompletionItemKind.TypeParameter);
-                                item.sortText = `${i}`;
-                                items.push(item);
-                            }
-                        }
-                    }
-
-                    return Promise.resolve(new CompletionList(items, false));  
-                }
+                // Map parameter data to completion items (if present)
+                inferData?.params[param].forEach((annotation, id) => {
+                    items.push(annotationToCompletionItem(annotation, id));
+                });
             }
         }
-        return Promise.resolve(null);
+
+        return Promise.resolve(new CompletionList(items, false));  
     }
 
     /**
@@ -115,19 +81,6 @@ export class ParamHintCompletionProvider extends CompletionProvider implements C
         const split = precedingText.split(/[,(*]/);
         let param = split.length > 1 ? split[split.length - 1].trim() : precedingText;
         return !param || /[!:?/\\{}.+/=)'";@&£%¤|<>$^~¨ -]/.test(param) ? null : param;
-    }
-    
-    private pushEstimationsToItems(typeHints: string[], items: CompletionItem[]) {
-
-        if (typeHints.length > 0) {
-            items.push(this.selectedCompletionItem(typeHints[0]));
-
-            for (let i = 1; i < typeHints.length; i++) {
-                let item = new CompletionItem(this.labelFor(typeHints[i]), CompletionItemKind.TypeParameter);
-                item.sortText = `${i}${typeHints[i]}`;
-                items.push(item);
-            }       
-        }
     }
 
     private shouldProvideItems(precedingText: string, activePos: Position, doc: TextDocument): boolean {
@@ -159,6 +112,7 @@ export class ReturnHintCompletionProvider extends CompletionProvider implements 
         token: CancellationToken,
         context: CompletionContext
     ): Promise<CompletionList | null> {
+        // Check if triggered with the correct return type annotation character
         if (context.triggerCharacter !== returnHintTrigger) {
             return null;
         }
@@ -168,16 +122,10 @@ export class ReturnHintCompletionProvider extends CompletionProvider implements 
         if (this.shouldProvideItems(line, pos)) {
             const inferData = findFunctionInferenceDataForActiveFilePos(pos);
             
-            if (inferData) {
-                for (let i = 0; i < inferData.returnTypes.length; ++i) {
-                    const item = new CompletionItem(
-                        inferData.returnTypes[i],
-                        CompletionItemKind.TypeParameter
-                    );
-                    item.sortText = `${i}`;
-                    items.push(item);
-                }
-            }
+            // Map return type data to completion items (if present)
+            inferData?.returnTypes.forEach((annotation, id) => {
+                items.push(annotationToCompletionItem(annotation, id));
+            });
         }
         return Promise.resolve(new CompletionList(items, false));
     }
@@ -202,6 +150,7 @@ export class ReturnHintCompletionProvider extends CompletionProvider implements 
         token: CancellationToken,
         context: CompletionContext
     ): Promise<CompletionList | null> {
+        // Check if triggered with the correct variable annotation character (same as param)
         if (context.triggerCharacter !== paramHintTrigger) {
             return null;
         }
@@ -210,19 +159,12 @@ export class ReturnHintCompletionProvider extends CompletionProvider implements 
         const line = doc.lineAt(pos);
 
         if (this.shouldProvideItems(line, pos)) {
-            // const inferData = findFunctionInferenceDataForActiveFilePos(pos);
             const inferData = findVariableInferenceDataForActiveFilePos(line);
             
-            if (inferData) {
-                for (let i = 0; i < inferData.annotations.length; ++i) {
-                    const item = new CompletionItem(
-                        inferData.annotations[i],
-                        CompletionItemKind.TypeParameter
-                    );
-                    item.sortText = `${i}`;
-                    items.push(item);
-                }
-            }
+            // Map variable data to completion items (if present)
+            inferData?.annotations.forEach((annotation, id) => {
+                items.push(annotationToCompletionItem(annotation, id));
+            });
         }
         return Promise.resolve(new CompletionList(items, false));
     }
@@ -266,9 +208,18 @@ function findFunctionInferenceDataForActiveFilePos(pos: Position): FunctionInfer
         }
     }
 
-    return undefined
+    return undefined;
 }
 
+/**
+ * Finds variable inference data for the specified position
+ * in the currently active file.
+ * 
+ * Returns undefined if such data could not be found.
+ * 
+ * @param line Line to query variable annotation data for
+ * @returns Variable inference data or undefined
+ */
 function findVariableInferenceDataForActiveFilePos(line: TextLine): VariableInferData | undefined {
     const activePath = window.activeTextEditor?.document.fileName;
 
@@ -299,3 +250,16 @@ function findVariableInferenceDataForActiveFilePos(line: TextLine): VariableInfe
     return undefined;
 }
 
+/**
+ * Helper function to transform an annotation at the given ID (lower = more confidence)
+ * to a CompletionItem to provide as autocomplete.
+ * 
+ * @param annotation Annotation (string)
+ * @param id Index
+ * @returns CompletionItem
+ */
+function annotationToCompletionItem(annotation: string, id: number): CompletionItem {
+    const item = new CompletionItem(annotation, CompletionItemKind.TypeParameter);
+    item.sortText = `${id}`;
+    return item;
+}

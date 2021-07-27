3858560957682005649d4f3ffec8d2f4161651ee
Evaldas Latoskinas <evaldasl61@gmail.com>
2021-07-27 13:42:24 +0300

    Add utils test for within line bounds

---
+++ test/suite/utils.test.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+import { isWithinLineBounds } from "../../src/utils";
+
+suite('Utils', () => {
+    describe('isWthinLineBounds tests', () => {
+
+        test('In bounds', () => {
+            assert.strictEqual(isWithinLineBounds(3, [1, 7]), true);
+        });
+
+        test('In bounds (left edge)', () => {
+            assert.strictEqual(isWithinLineBounds(2, [2, 4]), true);
+        });
+
+        test('In bounds (right edge)', () => {
+            assert.strictEqual(isWithinLineBounds(3, [1, 3]), true);
+        });
+
+        test('Out of bounds', () => {
+            assert.strictEqual(isWithinLineBounds(2, [6, 10]), false);
+        });
+
+        test('Out of bounds (left edge)', () => {
+            assert.strictEqual(isWithinLineBounds(3, [4, 6]), false);
+        });
+
+        test('Out of bounds (right edge)', () => {
+            assert.strictEqual(isWithinLineBounds(9, [2, 8]), false);
+        });
+    });
+});

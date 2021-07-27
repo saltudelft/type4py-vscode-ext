b4081ee7cff54172427657d8b784a3c747121884
Evaldas Latoskinas <evaldasl61@gmail.com>
2021-07-27 15:11:42 +0300

    Add additional mixed Python test file

---
+++ python/sample_files/various_cases.py
@@ -0,0 +1,14 @@
+class A():
+    t = 4
+
+    def __init__(self, p):
+        self.p = p
+    
+    def add(self, y):
+        return (self.p + y)
+
+def foo(x):
+    a = 1
+    return x + a
+
+v = 5

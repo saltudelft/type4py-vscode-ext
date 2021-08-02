6266e515f0e6496c4474663210d4bda056935ff8
Evaldas Latoskinas <evaldasl61@gmail.com>
2021-08-02 13:29:33 +0300

    Add error test cases for extension

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -3,9 +3,66 @@
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import * as chai from 'chai';
+import * as spies from 'chai-spies';
+import { ERROR_MESSAGES } from '../../src/messages';
+import { beforeEach } from 'mocha';
+import typestore from "../../src/typestore";
+
+chai.use(spies);
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
-    
+
+    beforeEach(() => {
+        chai.spy.restore();
+        chai.spy.on(vscode.window, 'showErrorMessage');
+    });
+
+    test("Infer without open file", async () => {
+        await vscode.commands.executeCommand("workbench.action.closeAllEditors");
+        await vscode.commands.executeCommand("type4py.infer");
+        chai.expect(vscode.window.showErrorMessage).to.have.been.called.with(ERROR_MESSAGES.noActiveFile);
+    });
+
+    test("Infer with empty file", async () => {
+        const doc = await vscode.workspace.openTextDocument({ language: "python", content: ""});
+        await vscode.window.showTextDocument(doc, { preview: false, viewColumn: 0 });
+        await vscode.commands.executeCommand("type4py.infer");
+
+        chai.expect(vscode.window.showErrorMessage).to.have.been.called.with(ERROR_MESSAGES.emptyFile);
+    });
+
+    test("Infer with non-Python file", async () => {
+        const doc = await vscode.workspace.openTextDocument({
+            language: "typescript", content: "const x = 5;"
+        });
+        await vscode.window.showTextDocument(doc, { preview: false, viewColumn: 0 });
+        await vscode.commands.executeCommand("type4py.infer");
+
+        chai.expect(vscode.window.showErrorMessage).to.have.been.called.with(ERROR_MESSAGES.nonPythonFile);
+    });
+
+    // test("Infer with Python file", async () => {
+    //     // const sourceFile: string = fs.readFileSync(
+            
+    //     // ).toString();
+    //     // const doc = await vscode.workspace.openTextDocument({ language: "python", content: sourceFile});
+    //     const fpath = path.resolve(__dirname, path.join("..", "res", "various_cases.py"));
+    //     const doc = await vscode.workspace.openTextDocument(fpath);
+    //     await vscode.window.showTextDocument(doc, { preview: false, viewColumn: 0 });
+
+    //     // Verify initially that inference data for the open document is unavailable
+    //     const activeDocument = vscode.window.activeTextEditor!.document.fileName;
+    //     chai.expect(typestore.get(activeDocument)).to.be.undefined;
+
+    //     // After inference, the data should now be available
+    //     await vscode.commands.executeCommand("type4py.infer");
+    //     chai.expect(typestore.get(activeDocument)).not.to.be.undefined;
+
+    //     // chai.expect(vscode.window.showErrorMessage).to.not.have.been.called();
+    // });
 });
